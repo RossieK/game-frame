cef3bcaddfe59d7ef3dd6f15a637686e641689e7
RossieK <rosi.kirova96@gmail.com>
2021-03-13 12:57:44 +0200

    Add styled games to home page

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,7 +3,6 @@ import Home from './pages/Home';
 function App() {
   return (
     <div className="App">
-      <h1>Hello From React!</h1>
       <Home />
     </div>
   );
--- front-end/src/pages/Home.js
+++ front-end/src/pages/Home.js
@@ -1,19 +1,51 @@
-import {useEffect} from 'react';
-import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import styled from 'styled-components';
+import { motion } from 'framer-motion';
 import { loadGames } from '../actions/gamesAction';
+import Game from '../components/Game';
 
 const Home = () => {
     //Fetch Data
     const dispatch = useDispatch();
-    useEffect(()=>{
-      dispatch(loadGames());
-    });
+    useEffect(() => {
+        dispatch(loadGames());
+    }, [dispatch]);
+
+    //Get data from state
+    const { popular, newGames, upcoming } = useSelector(state => state.games);
 
     return (
-        <div>
-            <h1>Home Page</h1>
-        </div>
+        <StyledGameList>
+            <h2>Upcoming Games</h2>
+            <StyledGames>
+                {upcoming.map(game => (
+                    <Game
+                        name={game.name}
+                        released={game.released}
+                        id={game.id}
+                        image={game.background_image}
+                        key={game.id}
+                    />
+                ))}
+            </StyledGames>
+        </StyledGameList>
     );
 };
+
+const StyledGameList = styled(motion.div)`
+    padding: 0rem 5rem;
+    h2{
+        padding: 5rem 0rem;
+    }
+`;
+
+const StyledGames = styled(motion.div)`
+    min-height: 80vh;
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(500px, 1fr));
+    grid-column-gap: 3rem;
+    grid-row-gap: 5rem;
+`;
 
 export default Home;
