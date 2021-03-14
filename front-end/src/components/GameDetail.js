333dd8f7cffc0f661f44977c6e10b11418fa0439
RossieK <rosi.kirova96@gmail.com>
2021-03-14 13:06:18 +0200

    Add platforms icons

--- front-end/src/components/GameDetail.js
+++ front-end/src/components/GameDetail.js
@@ -4,6 +4,13 @@ import { useSelector, useDispatch } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import { noDetailsLoaded } from '../actions/detailAction';
 import { smallImage } from '../util';
+//Images
+import playstation from '../img/playstation.svg';
+import steam from '../img/steam.svg';
+import xbox from '../img/xbox.svg';
+import nintendo from '../img/nintendo.svg';
+import apple from '../img/apple.svg';
+import gamepad from '../img/gamepad.svg';
 
 const GameDetail = () => {
     const history = useHistory();
@@ -17,6 +24,31 @@ const GameDetail = () => {
             document.body.style.overflow = 'auto';
             history.push('/');
             dispatch(noDetailsLoaded());
+        }
+    }
+
+    //Getting images
+    const getPlatform = (platform) => {
+        switch (platform) {
+            case "Playstation 4":
+                return playstation;
+
+            case "Playstation 5":
+                return playstation;
+
+            case "Xbox One":
+                return xbox;
+
+            case "PC":
+                return steam;
+
+            case "Nintendo Switch":
+                return nintendo;
+
+            case "iOS":
+                return apple;
+
+            default: return gamepad;
         }
     }
 
@@ -37,7 +69,7 @@ const GameDetail = () => {
                                 <h3>Platforms</h3>
                                 <StyledPlatforms>
                                     {game.platforms.map(data => (
-                                        <h3 key={data.platform.id}>{data.platform.name}</h3>
+                                        <img alt={data.platform.name} src={getPlatform(data.platform.name)} key={data.platform.id}></img>
                                     ))}
                                 </StyledPlatforms>
                             </StyledInfo>
