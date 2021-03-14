a761573a07985f6955a106e7b63791bcab0877e0
RossieK <rosi.kirova96@gmail.com>
2021-03-14 16:54:39 +0200

    Add navbar with styling

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,12 +1,14 @@
 import { Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Nav from './components/Nav';
+import Navbar from './components/Navbar';
 import GlobalStyles from './components/GlobalStyle';
 
 function App() {
   return (
     <div className="App">
       <GlobalStyles />
+      <Navbar />
       <Nav />
       <Route path={["/game/:id", "/"]}>
         <Home />
---
+++ front-end/src/components/Navbar.js
@@ -0,0 +1,116 @@
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
+import { useLocation } from 'react-router-dom';
+import logo from '../img/logo.svg';
+
+const Navbar = () => {
+    const { pathname } = useLocation();
+
+    return (
+        <StyledNavbar>
+            <h1 id="logo"><img src={logo} alt="logo" /><Link to="/">Game Frame</Link></h1>
+            <ul>
+                <li>
+                    <Link to="/">All Games</Link>
+                    <StyledLine transition={{ duration: 0.75 }} initial={{ width: "0%" }} animate={{ width: pathname === "/" ? "50%" : "0%" }} />
+                </li>
+                <li>
+                    <Link to="/myprofile">My profile</Link>
+                    <StyledLine transition={{ duration: 0.75 }} initial={{ width: "0%" }} animate={{ width: pathname === "/myprofile" ? "50%" : "0%" }} />
+                </li>
+                <li>
+                    <Link to="/logout">Logout</Link>
+                    <StyledLine transition={{ duration: 0.75 }} initial={{ width: "0%" }} animate={{ width: pathname === "/logout" ? "50%" : "0%" }} />
+                </li>
+                <li>
+                    <Link to="/register">Register</Link>
+                    <StyledLine transition={{ duration: 0.75 }} initial={{ width: "0%" }} animate={{ width: pathname === "/register" ? "50%" : "0%" }} />
+                </li>
+                <li>
+                    <Link to="/login">Login</Link>
+                    <StyledLine transition={{ duration: 0.75 }} initial={{ width: "0%" }} animate={{ width: pathname === "/login" ? "50%" : "0%" }} />
+                </li>
+            </ul>
+        </StyledNavbar>
+    );
+};
+
+const StyledNavbar = styled.div`
+    min-height:10vh;
+    display:flex;
+    margin:auto;
+    justify-content:space-between;
+    align-items:center;
+    padding:0rem 2.5rem;
+    background: #f18d19;
+    position:sticky;
+    top:0;
+    z-index:10;
+
+    a{
+    color:white;
+    font-size: 1.3rem;
+    font-family: 'Orbitron', sans-serif;
+    text-decoration:none;
+    }
+
+    ul{
+    display:flex;
+    list-style:none;
+    }
+
+    #logo{
+    display: flex;
+    justify-content: center;
+    padding: 1rem;
+    cursor: pointer;
+        img{
+        height: 4rem;
+        width: 4rem;
+        }
+
+        a{
+        font-size:3.5rem;
+        font-family: 'Jura', sans-serif;
+        color:black;
+        }
+    }
+
+    li{
+    padding-left:8rem;
+    position:relative;
+    }
+
+    @media (max-width:1300px){
+        flex-direction:column;
+        padding: 2rem 1rem;
+        
+        #logo{
+            display:inline-block;
+            margin:2rem;
+        }
+        ul{
+            padding:2rem;
+            justify-content:space-around;
+            width:100%;
+            li{
+                padding:0;
+            }
+        }
+    }
+`;
+
+const StyledLine = styled(motion.div)`
+height:0.3rem;
+background:#303548;
+width:0%;
+position:absolute;
+bottom:-80%;
+left:53%;
+@media (max-width:1300px){
+    left: 0%;
+}
+`;
+
+export default Navbar;
