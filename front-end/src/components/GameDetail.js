4150155cd953e186e781fec983a54eb490b78d33
RossieK <rosi.kirova96@gmail.com>
2021-03-14 13:15:41 +0200

    Add rating stars

--- front-end/src/components/GameDetail.js
+++ front-end/src/components/GameDetail.js
@@ -11,6 +11,8 @@ import xbox from '../img/xbox.svg';
 import nintendo from '../img/nintendo.svg';
 import apple from '../img/apple.svg';
 import gamepad from '../img/gamepad.svg';
+import starEmpty from '../img/star-empty.png';
+import starFull from '../img/star-full.png';
 
 const GameDetail = () => {
     const history = useHistory();
@@ -28,6 +30,20 @@ const GameDetail = () => {
     }
 
     //Getting images
+    const getStars = () => {
+        const stars = [];
+        const rating = Math.floor(game.rating);
+        for (let i = 1; i <= 5; i++) {
+            if (i <= rating) {
+                stars.push(<img alt="star" key={i} src={starFull}></img>);
+            } else {
+                stars.push(<img alt="star" key={i} src={starEmpty}></img>);
+            }
+        }
+
+        return stars;
+    }
+
     const getPlatform = (platform) => {
         switch (platform) {
             case "Playstation 4":
@@ -64,6 +80,7 @@ const GameDetail = () => {
                             <div className="rating">
                                 <h3>{game.name}</h3>
                                 <p>Rating: {game.rating}</p>
+                                {getStars()}
                             </div>
                             <StyledInfo>
                                 <h3>Platforms</h3>
@@ -131,6 +148,11 @@ const StyledStats = styled(motion.div)`
     display: flex;
     align-items: center;
     justify-content: space-between;
+    img{
+        width: 2rem;
+        height: 2rem;
+        display: inline;
+    }
 `;
 
 const StyledInfo = styled(motion.div)`
