566b5f44f1b2c5dc66ab5996b88cf35d59d6838d
RossieK <rosi.kirova96@gmail.com>
2021-03-24 20:28:07 +0200

    Add styled content to home guest page

--- front-end/src/components/HomeGuest.js
+++ front-end/src/components/HomeGuest.js
@@ -1,18 +1,34 @@
+//Functionality
+import { Link } from 'react-router-dom';
 //Styling
 import styled from 'styled-components';
 import { motion } from 'framer-motion';
 import { fadeIn } from '../animations';
+//Image
+import backgroundImg from '../img/background.jpeg';
 
 const HomeGuest = () => {
     return (
         <StyledGuestHome variants={fadeIn} initial="hidden" animate="show">
-           <h1>Hello Guest!</h1>
+            <h1>Please <Link to="/register">Register</Link> / <Link to="/login">Login</Link> to access content</h1>
+            <img src={backgroundImg} alt="Games"/>
         </StyledGuestHome>
     );
 };
 
 const StyledGuestHome = styled(motion.div)`
+    h1{
+        color: white;
+        font-family: 'Orbitron', sans-serif;
+        text-align: center;
+        margin-top: 5rem;
+        margin-bottom: 5rem;
+    }
 
+    img{
+        width:60%;
+        margin: auto;
+    }
 `;
 
 export default HomeGuest;
